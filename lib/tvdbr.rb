25a2a1192b0f53abffa078ea8d4c2df862ea2994
Nathan Esquenazi <nesquena@gmail.com>
2011-01-17 21:02:57 -0800

    Autoload various tvdbr classes in tvdbr.rb

--- lib/tvdbr.rb
+++ lib/tvdbr.rb
@@ -1,3 +1,6 @@
 module Tvdbr
-  # Your code goes here...
+  autoload :Client,    'tvdbr/client'
+  autoload :DataSet,   'tvdbr/data_set'
+  autoload :Series,    'tvdbr/series'
+  autoload :Episode,   'tvdbr/episode'
 end
