a2922f1506ec193ee4f1bfac4671ffce7cb59d83
Nathan Esquenazi <nesquena@gmail.com>
2011-01-17 21:28:17 -0800

    Fixes demo file in examples

--- examples/demo.rb
+++ examples/demo.rb
@@ -1,6 +1,7 @@
+require 'rubygems'
 require 'tvdbr'
 
-tvdb = Tvdbr::Client.new('8C8719A1466ECF7A')
+tvdb = Tvdbr::Client.new('YOUR_API_KEY')
 
 # Fetch series based on title and starring
 series_wrong = tvdb.fetch_series_from_data(:title => "Dexter",
@@ -20,21 +21,21 @@ puts series.starring.join(", ")
 puts series.poster
 puts series.release_date.inspect
 
-## Fetch episodes for a series
-#puts "\nEpisodes:\n\n"
-#series.episodes.each do |e|
-# puts e.name + " (S#{e.season_num}E#{e.episode_num})"
-#end
+# Fetch episodes for a series
+puts "\nEpisodes:\n\n"
+series.episodes.each do |e|
+ puts e.name + " (S#{e.season_num}E#{e.episode_num})"
+end
+
+#  # Fetch series updates since given timestamp
+#  results = tvdb.find_updates_since(2.hours.ago)
+#  puts "\n\nUpdated series since 2 hours ago: #{results[:series].size} series\n\n"
+#  tvdb.each_updated_series(:since => 2.hours.ago) do |series|
+#    puts "#{series.id} - #{series.title}"
+#  end
 #
-## Fetch series updates since given timestamp
-#results = tvdb.find_updates_since(2.hours.ago)
-#puts "\n\nUpdated series since 2 hours ago: #{results[:series].size} series\n\n"
-#tvdb.each_updated_series(:since => 2.hours.ago) do |series|
-#  puts "#{series.id} - #{series.title}"
-#end
-#
-## Fetch episode udpates since given timestamp
-#puts "Updated episodes since 2 hours ago: #{results[:episodes].size} episodes\n\n"
-#tvdb.each_updated_episode(:since => 2.hours.ago) do |episode|
-#  puts "#{episode.id} - #{episode.name} - #{episode.seriesid}"
-#end
+#  # Fetch episode udpates since given timestamp
+#  puts "Updated episodes since 2 hours ago: #{results[:episodes].size} episodes\n\n"
+#  tvdb.each_updated_episode(:since => 2.hours.ago) do |episode|
+#    puts "#{episode.id} - #{episode.name} - #{episode.seriesid}"
+#  end
