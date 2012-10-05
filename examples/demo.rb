c8b5a27416188962ff5f8116bbbde90df0714b86
Nathan Esquenazi <nesquena@gmail.com>
2012-10-05 01:59:10 -0700

    Update demo file and add new demo2

--- examples/demo.rb
+++ examples/demo.rb
@@ -2,16 +2,16 @@ require 'rubygems'
 require 'tvdbr'
 require 'active_support/all'
 
-tvdb = Tvdbr::Client.new('YOUR_API_KEY')
+tvdb = Tvdbr::Client.new('5FEC454623154441')
 
 # Fetch series based on title and starring
 series_wrong = tvdb.fetch_series_from_data(:title => "Dexter",
-  :starring => "Adewale Akinnuoye-Agbaje, Sam Anderson, Naveen Andrews, L. Scott Caldwell, Nestor Carbonell, François Chau, Henry Ian Cusick, Jeremy Davies, Emilie de Ravin, Michael Emerson, Jeff Fahey, Fionnula Flanagan, Matthew Fox, Jorge Garcia, Maggie Grace, Josh Holloway, Malcolm David Kelley, Daniel Dae Kim, Yunjin Kim, Ken Leung, Evangeline Lilly, Rebecca Mader, Elizabeth Mitchell, Dominic Monaghan, Terry O'Quinn, Harold Perrineau, Zuleikha Robinson, Michelle Rodriguez, Kiele Sanchez,Rodrigo Santoro, Ian Somerhalder, John Terry, Sonya Walger, Cynthia Watros")
+  :starring => "Adewale Akinnuoye-Agbaje, Sam Anderson, Naveen Andrews, L. Scott Caldwell, Nestor Carbonell, Francois Chau, Henry Ian Cusick, Jeremy Davies, Emilie de Ravin, Michael Emerson, Jeff Fahey, Fionnula Flanagan, Matthew Fox, Jorge Garcia, Maggie Grace, Josh Holloway, Malcolm David Kelley, Daniel Dae Kim, Yunjin Kim, Ken Leung, Evangeline Lilly, Rebecca Mader, Elizabeth Mitchell, Dominic Monaghan, Terry O'Quinn, Harold Perrineau, Zuleikha Robinson, Michelle Rodriguez, Kiele Sanchez,Rodrigo Santoro, Ian Somerhalder, John Terry, Sonya Walger, Cynthia Watros")
 puts "Wrong Match: #{series_wrong.inspect}"
 
 # Fetch series based on title and starring
 series_right = tvdb.fetch_series_from_data(:title => "Lost",
-  :starring => "Adewale Akinnuoye-Agbaje, Sam Anderson, Naveen Andrews, L. Scott Caldwell, Nestor Carbonell, François Chau, Henry Ian Cusick, Jeremy Davies, Emilie de Ravin, Michael Emerson, Jeff Fahey, Fionnula Flanagan, Matthew Fox, Jorge Garcia, Maggie Grace, Josh Holloway, Malcolm David Kelley, Daniel Dae Kim, Yunjin Kim, Ken Leung, Evangeline Lilly, Rebecca Mader, Elizabeth Mitchell, Dominic Monaghan, Terry O'Quinn, Harold Perrineau, Zuleikha Robinson, Michelle Rodriguez, Kiele Sanchez,Rodrigo Santoro, Ian Somerhalder, John Terry, Sonya Walger, Cynthia Watros")
+  :starring => "Adewale Akinnuoye-Agbaje, Sam Anderson, Naveen Andrews, L. Scott Caldwell, Nestor Carbonell, Francois Chau, Henry Ian Cusick, Jeremy Davies, Emilie de Ravin, Michael Emerson, Jeff Fahey, Fionnula Flanagan, Matthew Fox, Jorge Garcia, Maggie Grace, Josh Holloway, Malcolm David Kelley, Daniel Dae Kim, Yunjin Kim, Ken Leung, Evangeline Lilly, Rebecca Mader, Elizabeth Mitchell, Dominic Monaghan, Terry O'Quinn, Harold Perrineau, Zuleikha Robinson, Michelle Rodriguez, Kiele Sanchez,Rodrigo Santoro, Ian Somerhalder, John Terry, Sonya Walger, Cynthia Watros")
 puts "Right Match: #{series_right.inspect}"
 
 puts "\n\n"
@@ -28,16 +28,16 @@ series.episodes.each do |e|
  puts e.name + " (S#{e.season_num}E#{e.episode_num})"
 end
 
- # Fetch series updates since given timestamp
- time = 10.minutes.ago
- results = tvdb.find_updates_since(time)
- puts "\n\nUpdated series since 10 mins ago: #{results[:series].size} series\n\n"
- tvdb.each_updated_series(:since => time) do |series|
-   puts "#{series.id} - #{series.title}"
- end
+# Fetch series updates since given timestamp
+time = 10.minutes.ago
+results = tvdb.find_updates_since(time)
+puts "\n\nUpdated series since 10 mins ago: #{results[:series].size} series\n\n"
+tvdb.each_updated_series(:since => time) do |series|
+ puts "#{series.id} - #{series.title}"
+end
 
- # Fetch episode udpates since given timestamp
- puts "\nUpdated episodes since 10 mins ago: #{results[:episodes].size} episodes\n\n"
- tvdb.each_updated_episode(:since => time) do |episode|
-   puts "#{episode.id} - #{episode.name} - #{episode.seriesid}"
- end
+# Fetch episode udpates since given timestamp
+puts "\nUpdated episodes since 10 mins ago: #{results[:episodes].size} episodes\n\n"
+tvdb.each_updated_episode(:since => time) do |episode|
+ puts "#{episode.id} - #{episode.name} - #{episode.seriesid}"
+end
---
+++ examples/demo2.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'tvdbr'
+require 'active_support/all'
+
+# Updated APIs circa 10/05/2012
+tvdb = Tvdbr::Client.new('5FEC454623154441')
+
+# Find episode with airdate
+episode = tvdb.find_episode_by_airdate(80348, '2007-09-24')
+puts episode.inspect
+
+# Find with remote id
+series = tvdb.find_series_by_remote_id('tt0290978')
+puts series.inspect
+
+# Fetch using static updates instead of since
+# Fetch series updates since given timestamp
+results = tvdb.find_static_updates(:day)
+puts "\n\nUpdated series today: #{results[:series].size} series\n\n"
+tvdb.each_updated_series(:period => :day) do |series|
+ puts "#{series.id} - #{series.title}"
+end
+
+# Fetch episode udpates since given timestamp
+puts "\nUpdated episodes since 10 mins ago: #{results[:episodes].size} episodes\n\n"
+tvdb.each_updated_episode(:period => :day) do |episode|
+ puts "#{episode.id} - #{episode.name} - #{episode.seriesid}"
+end
